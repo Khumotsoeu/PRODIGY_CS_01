adeaf01824c2530b987ec9a25ecb42625a572777
khtsoeujhb024 <khtsoeujhb024@student.wethinkcode.co.za>
2025-07-07 08:29:01 +0200

    test program

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,12 +1,33 @@
 def encrypt(text, shift):
     result = ""
     for character in text:
-           if character.isalpha():
-               shift_base = ord("A") if character.isupper() else ord("a")
-               result += character((ord(character) - shift_base + shift) % 26 + shift_base)
-           else:
-                   result += character
+        if character.isalpha():
+            shift_base = ord("A") if character.isupper() else ord("a")
+            result += character((ord(character) - shift_base + shift) % 26 + shift_base)
+        else:
+            result += character
     return result
 
 def decrypt(text, shift):
-      return encrypt(text, -shift)
+    return encrypt(text, -shift)
+
+#the main program
+def main():
+    print("Welcome to a Ceaser Cipher Program!")
+    choice = input("would you like to encrypt or decrypt? e/d: ").lower()
+    message = input("Enter your message: ")
+    shift = int(input("Enter shift value (number):"))
+
+    if choice == 'e':
+        encrypted = encrypt(message, shift)
+        print("Encrypt Message:", encrypted)
+    elif choice == 'd':
+        decrypted = decrypt(message, shift)
+        print("Decrypted Message:", decrypted)
+    else:
+        print("Invalid choice. Please enter 'e' or 'd'.")
+
+if __name__=="__main__":
+     main()
+
+    
